42f2a8af626f47f48a1787f4735c3065b1d44a04
Grantdit <kiruha5854@mail.ru>
2022-10-21 01:28:02 +0300

    Домашнее задание

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,73 @@
 public class Main {
     public static void main(String[] args) {
-        var box = 5;
-        System.out.println(box);
-        box = box + 2;
-        System.out.println(box);
-        box = box - 3;
-        System.out.println(box);
-        box = box * 3;
-        System.out.println(box);
-        box = box / 4;
-        System.out.println(box
-        );
-    }
-}
+        var dog = 8;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+        dog = dog + 4;
+        System.out.println(dog);
+        cat = cat + 4;
+        System.out.println(cat);
+        paper = paper + 4;
+        System.out.println(paper);
+        dog = dog - 3;
+        System.out.println(dog);
+        cat = cat - 1.6;
+        System.out.println(cat);
+        paper = paper - 7639;
+        System.out.println(paper);
 
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+
+        var boxer1 = 78.2;
+        System.out.println(boxer1);
+        var boxer2 = 82.7;
+        System.out.println(boxer2);
+        var general = boxer1 + boxer2;
+        System.out.println(general);
+        var difference = boxer2-boxer1;
+        System.out.println(difference);
+        difference = boxer2 % boxer1;
+        System.out.println(difference);
+
+        var Hours = 640;
+        System.out.println("общее число часов:"+ Hours);
+        float hours = 8;
+        System.out.println("тратит один человек:" + hours);
+        var people = Hours / hours;
+        System.out.println("количесво людей:"+ people);
+        people = people + 94;
+        System.out.println("новое количество людей:"+ people);
+        var hours2 = 1.*Hours / people;
+        System.out.println("сколько должен тратить 1 человек:" + hours2);
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
 
